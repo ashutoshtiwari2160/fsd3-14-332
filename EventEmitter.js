11925da54c0a5243ae9a0d7a37b297a4930ec5b1
Ashutosh Tiwari <ashutoshtiwari2160@gmail.com>
2026-07-21 10:26:19 +0530

    initial commit

---
+++ EventEmitter.js
@@ -0,0 +1,26 @@
+import {EventEmitter} from 'node:events'
+
+const sayHi = (name) => { 
+  console.log('$(name) logged in') ;
+};
+const task = new EventEmitter () ;
+task.once("greet",()=>{
+console.log("system started") ;
+});
+task.on("greet",sayHi) ;
+task.on("greet",(name)=>){
+  console.log($(name) starts working ) ;
+}
+task.once("exit",name) => {
+  console.log(system shutdown by $(name){
+   
+  });
+}
+task.emit("greet","Rahul Singh") ;
+console.log() ;
+task.off("greet",sayHI);
+task.emit("greet"."Manish Sinha");
+task.emit("greet","Mukesh Gupta") ;
+console.log() ;
+task.emit("exit","Manager");//execute only once
+task.emit("exit","Employee")
